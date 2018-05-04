72f76804dda014c616fb2101978f09a50ea6395b
Alexandra Usacheva <lazercore@me.com>
2018-05-04 16:18:37 +0300

    FE-599: updated

--- src/app/components/app/modal-container/modal/info/info.tsx
+++ src/app/components/app/modal-container/modal/info/info.tsx
@@ -8,10 +8,10 @@ export const Info: React.SFC = () => (
     <div className={styles.info}>
         <div>
             <h4 className={styles.company_name} id='company-name-label'>{getIdentityChallengeTitle()}</h4>
-            <div>
-                <div className={styles.order}>Описание</div>
-                <div className={styles.product_description} id='product-description'>Што происходит?</div>
-            </div>
+            {/*<div>*/}
+                {/*<div className={styles.order}>Описание</div>*/}
+                {/*<div className={styles.product_description} id='product-description'>Што происходит?</div>*/}
+            {/*</div>*/}
         </div>
     </div>
 );
