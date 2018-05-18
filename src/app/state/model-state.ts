e439302e6a96434d9fed2a2701640256af0c82a6
Alexandra Usacheva <lazercore@me.com>
2018-05-18 15:27:07 +0300

    FE-603: model reducer

--- src/app/state/model-state.ts
+++ src/app/state/model-state.ts
@@ -1,11 +1,5 @@
 import { Identity } from 'app/backend/model/identity';
 
-export enum ModelStatus {
-    initialized = 'initialized',
-    none = 'none'
-}
-
 export class ModelState {
     identity?: Identity;
-    status: ModelStatus;
 }
