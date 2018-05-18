f525f9780a7f22c4ebf80b7803981bd24931e32d
Alexandra Usacheva <lazercore@me.com>
2018-05-18 19:07:23 +0300

    FE-603: PR update

--- src/is-in-iframe.ts
+++ src/is-in-iframe.ts
@@ -1,6 +1,6 @@
 export const isInFrame = (): boolean => {
     try {
-        return !(window.self === window.top);
+        return window.self !== window.top;
     } catch (e) {
         return true;
     }
